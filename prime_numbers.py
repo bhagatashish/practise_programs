24592f0ab48bf37be9eb41074610449c9ab83768
ashish <bhagat.ashish09@gmail.com>
2022-05-18 03:01:34 +0100

    new updates

--- prime_numbers.py
+++ prime_numbers.py
@@ -5,11 +5,25 @@ Created on Jul 2, 2019
 '''
 #:Java Program to Find the Largest Two Numbers in a Given Array
 list = [12,23,1,2,8,40,55]
+list1 = []
+
+largest_num = list[0]
+# first way to get largest num
+for num in list:
+    if num > largest_num:
+        largest_num = num
+print(largest_num)
+
+# using python built in function
+print(f"the larget number is {max(list)}")
+
+# for 2nd largest num
 list.sort()
-print(list)
-len = len(list)-1
-print (list[len])
-print(list[len-1])
-for i in range (len(list)):
-    print(list[i])
+print(f"the 2nd largest num is {list[-2]}")
+
+
+
+
+
+
     
